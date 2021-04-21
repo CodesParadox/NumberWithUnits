521aed7e2a924e1b89c9239eadca22e6942bdb3b
Gal Braymok <69432977+CodesParadox@users.noreply.github.com>
2021-04-21 20:11:46 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 using namespace ariel;
 
-const string test_file = "./units.txt";
+//const string test_file = "./units.txt";
 /* 
 1 km = 1000 m
 1 m = 100 cm
@@ -18,7 +18,7 @@ const string test_file = "./units.txt";
 
 TEST_CASE("Read_units test")
 {
-
+	ifstream units_file{"units.txt"};
 	CHECK(NumberWithUnits(1, "g") == NumberWithUnits(1, "g"));
 	CHECK(NumberWithUnits(1, "hour") == NumberWithUnits(1, "hour"));
 	CHECK(NumberWithUnits(1, "min") == NumberWithUnits(1, "min"));
@@ -41,6 +41,7 @@ TEST_CASE("Read_units test")
 
 TEST_CASE("Checking operators ")
 {
+	ifstream units_file{"units.txt"};
 	NumberWithUnits km(2, "km");
 	NumberWithUnits m1(1000, "km");
 	NumberWithUnits m(500, "m");
@@ -62,7 +63,7 @@ TEST_CASE("Checking operators ")
 
 TEST_CASE("operator +")
 {
-	ifstream units_file{test_file};
+	ifstream units_file{"units.txt"};
 	NumberWithUnits a(3, "m");
 	NumberWithUnits b(-40, "cm");
 	NumberWithUnits c(5, "sec");
@@ -79,32 +80,19 @@ TEST_CASE("operator +")
 
 TEST_CASE("OPERTOR +=")
 {
-	ifstream units_file{test_file};
+	ifstream units_file{"units.txt"};
 	NumberWithUnits::read_units(units_file);
 	NumberWithUnits a1(3, "m"), a2(40, "cm"), a3(3.4, "m");
 	CHECK((a1 += a2) == a3);
-
 	NumberWithUnits b1(1, "g"), b2(1, "kg"), b3(1001, "g");
 	CHECK((b1 += b2) == b3);
-
 	NumberWithUnits c1(1, "kg"), c2(1, "g"), c3(1.001, "kg");
 	CHECK((c1 += c2) == c3);
-
-	NumberWithUnits d1(1, "g"), d2(1, "ton"), d3(1000001, "g");
-	CHECK((d1 += d2) == d3);
-
-	NumberWithUnits e1(1, "ton"), e2(1, "g"), e3(1.000001, "ton");
-	CHECK((e1 += e2) == e3);
-
-	NumberWithUnits f1(3, "m"), f2(5, "sec");
-	CHECK_THROWS_MESSAGE(f1 += f2, "Types not from the same system!");
 }
 
-//============================= for operator - ========================//
-
-TEST_CASE("OPERTOR -")
+TEST_CASE("OPERTOR check check")
 {
-	ifstream units_file{test_file};
+	ifstream units_file{"units.txt"};
 	NumberWithUnits::read_units(units_file);
 	CHECK((NumberWithUnits(3, "m") - NumberWithUnits(40, "cm")) == NumberWithUnits(2.6, "m"));
 	CHECK((NumberWithUnits(1, "kg") - NumberWithUnits(1, "g")) == NumberWithUnits(0.999, "kg"));
@@ -116,62 +104,19 @@ TEST_CASE("OPERTOR -")
 	CHECK_THROWS_MESSAGE(NumberWithUnits(5, "g") - NumberWithUnits(3, "km"), "Unit not from the same system");
 }
 
-TEST_CASE("OPERTOR -=")
+TEST_CASE("OPERTOR >=<")
 {
-	ifstream units_file{test_file};
+	ifstream units_file{"units.txt"};
 	NumberWithUnits::read_units(units_file);
-	NumberWithUnits a1(3, "m"), a2(40, "cm"), a3(2.6, "m");
-	CHECK((a1 -= a2) == a3);
-
-	NumberWithUnits b1(1, "g"), b2(1, "kg"), b3(-999, "g");
-	CHECK((b1 -= b2) == b3);
-
-	NumberWithUnits c1(1, "kg"), c2(1, "g"), c3(0.999, "kg");
-	CHECK((c1 -= c2) == c3);
-
-	NumberWithUnits d1(1, "g"), d2(1, "ton"), d3(-999999, "g");
-	CHECK((d1 -= d2) == d3);
-
-	NumberWithUnits e1(1, "ton"), e2(1, "g"), e3(0.999999, "ton");
-	CHECK((e1 -= e2) == e3);
-
-	NumberWithUnits f1(3, "m"), f2(5, "sec");
-	CHECK_THROWS_MESSAGE(f1 -= f2, "Types not from the same system!");
-}
-
-//==============================  comper ==========================================//
-
-TEST_CASE("OPERTOR >")
-{
-	ifstream units_file{test_file};
-	NumberWithUnits::read_units(units_file);
-	CHECK(NumberWithUnits(6, "kg") > NumberWithUnits(5, "kg"));
-	CHECK(NumberWithUnits(3.5, "ton") > NumberWithUnits(3000, "kg"));
-	CHECK(NumberWithUnits(7000, "m") > NumberWithUnits(6, "km"));
-	CHECK(NumberWithUnits(5001, "g") > NumberWithUnits(5, "kg"));
-	CHECK_FALSE(NumberWithUnits(1, "hour") > NumberWithUnits(60.1, "min"));
+	CHECK_FALSE(NumberWithUnits(100, "ILS") > NumberWithUnits(5, "USD"));
 	CHECK(NumberWithUnits(6, "kg") >= NumberWithUnits(5, "kg"));
-	CHECK(NumberWithUnits(3, "ton") >= NumberWithUnits(3000, "kg"));
-	CHECK(NumberWithUnits(7000, "m") >= NumberWithUnits(6, "km"));
-	CHECK(NumberWithUnits(5001, "g") >= NumberWithUnits(5, "kg"));
+	CHECK(NumberWithUnits(3, "ton") >= NumberWithUnits(2000, "kg"));
+	CHECK(NumberWithUnits(6001, "m") >= NumberWithUnits(6, "km"));
+	CHECK(NumberWithUnits(399991, "g") >= NumberWithUnits(5, "kg"));
 	CHECK(NumberWithUnits(1, "hour") >= NumberWithUnits(60, "min"));
-	CHECK_FALSE(NumberWithUnits(1, "hour") >= NumberWithUnits(60.1, "min"));
-	ifstream units_file{test_file};
-	NumberWithUnits::read_units(units_file);
-	CHECK(NumberWithUnits(5, "kg") < NumberWithUnits(6, "kg"));
-	CHECK(NumberWithUnits(3000, "kg") < NumberWithUnits(3.5, "ton"));
-	CHECK(NumberWithUnits(6, "km") < NumberWithUnits(7000, "m"));
-	CHECK(NumberWithUnits(5, "kg") < NumberWithUnits(5001, "g"));
-	CHECK_FALSE(NumberWithUnits(60.1, "min") < NumberWithUnits(1, "hour"));
-}
-
-TEST_CASE("OPERTOR <=")
-{
-	ifstream units_file{test_file};
-	NumberWithUnits::read_units(units_file);
 	CHECK(NumberWithUnits(5, "kg") <= NumberWithUnits(6, "kg"));
-	CHECK(NumberWithUnits(3, "kg") <= NumberWithUnits(3, "kg"));
-	CHECK(NumberWithUnits(6, "km") <= NumberWithUnits(7000, "m"));
+	CHECK_FALSE(NumberWithUnits(1, "hour") >= NumberWithUnits(60.1, "min"));
+	CHECK(NumberWithUnits(5, "kg") < NumberWithUnits(6, "kg"));
+	CHECK(NumberWithUnits(53, "ILS") < NumberWithUnits(51, "USD"));
 	CHECK(NumberWithUnits(5, "kg") <= NumberWithUnits(5000, "g"));
-	CHECK_FALSE(NumberWithUnits(7, "min") <= NumberWithUnits(5, "min"));
 }
